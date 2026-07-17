b68276d6ce818042bffb7e87c4a6efa2f6e28803
Naimul Hasan Nayeem <151813315+nayxctl@users.noreply.github.com>
2026-07-18 00:56:09 +0600

    Add sumAllOdd.cpp to calculate odd perfect squares
    Refactor program to sum all odd perfect squares up to n.

--- Loop/sumAllOdd.cpp
+++ Loop/sumAllOdd.cpp
@@ -4,22 +4,24 @@ For example, if the user inputs the number I00, the program should print out 165
 */
 
 #include<iostream>
+#include<cmath>
+
 using namespace std;
 
 int main(){
-    float planet_mass;
-    float total_mass = 0;
-    int n = 0;
 
-    while(n < 8){
-        cout << "Enter the mass of planet " << (n+1) << " (in kg): ";
-        cin >> planet_mass;
+     int x,n,sum=0;
+     cout << "Enter a value of n: ";
+     cin >> n;
 
-        total_mass = total_mass + planet_mass;
-        n++;
-    }
-
-    cout << "\nTotal mass of the solar system's planets is: " << total_mass << " kg\n";
-
-    return 0;
+     for(x=1;n>=x;x++){
+        if(x%2!=0){
+            int root = sqrt(x);
+            if (root*root==x){
+                sum=sum+x;
+            }
+        }
+     }
+     cout << sum;
+     return 0;
 }
